f1cfa095003078ba721953d3b7762ec8dbabb91b
Stephan Maka <astro@osdyne.com>
2025-01-23 04:21:46 +0100

    Add record size limit data types

--- src/extensions/extension_data/mod.rs
+++ src/extensions/extension_data/mod.rs
@@ -2,6 +2,7 @@ pub mod key_share;
 pub mod max_fragment_length;
 pub mod pre_shared_key;
 pub mod psk_key_exchange_modes;
+pub mod record_size_limit;
 pub mod server_name;
 pub mod signature_algorithms;
 pub mod signature_algorithms_cert;
